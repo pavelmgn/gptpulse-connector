bd2170da3d945a4972d46b65758cc26288e8e560
Павел <pavelmgn@gmail.com>
2024-04-05 11:58:03 +0500

    feat: MqRequestType consts

---
+++ src/Constants/MqRequestType.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pavelmgn\GptPulseConnector\Constants;
+
+final class MqRequestType
+{
+    public const REQUEST = 1;
+    public const BASE_REQUEST = 2;
+}
