64bf0c1079206cd1bb33e3805bea88f4a7f0b40f
Павел <pavelmgn@gmail.com>
2024-04-09 16:09:58 +0500

    feat: knowledge_target_request

--- src/Constants/MqRequestType.php
+++ src/Constants/MqRequestType.php
@@ -6,22 +6,25 @@ namespace Pavelmgn\GptPulseConnector\Constants;
 
 final class MqRequestType
 {
-    public const KNOWLEDGE_REQUEST = 1;
-    public const BASE_REQUEST      = 2;
+    public const KNOWLEDGE_REQUEST        = 1;
+    public const BASE_REQUEST             = 2;
+    public const KNOWLEDGE_TARGET_REQUEST = 3;
 
     public static function getList(): array
     {
         return [
             self::KNOWLEDGE_REQUEST,
             self::BASE_REQUEST,
+            self::KNOWLEDGE_TARGET_REQUEST,
         ];
     }
 
     public static function getListDescription(): array
     {
         return [
-            self::KNOWLEDGE_REQUEST => 'Обработка данных для формирования ответа основываясь на данных в БЗ',
-            self::BASE_REQUEST      => 'Обработка вопроса и ответа для предоставления базового',
+            self::KNOWLEDGE_REQUEST        => 'Обработка данных для формирования ответа основываясь на данных в БЗ',
+            self::BASE_REQUEST             => 'Обработка вопроса и ответа для предоставления базового',
+            self::KNOWLEDGE_TARGET_REQUEST => 'Обработка данных для формирования ответа основываясь на данных в БЗ по id вопроса(ов)',
         ];
     }
 }
